f826b4bbfb85455b4fdc057f80b098f8b2a1dece
prakhar362 <prakharshri2005@gmail.com>
2025-01-05 22:05:27 +0530

    basic backend setup

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config(); // Load environment variables
+const mongoose = require("mongoose");
+
+
+const app = express();
+const port=process.env.PORT;
+async function main()
+{
+    try{
+        //using dotenv
+    const mongourl=process.env.MONGO_URL;
+    await mongoose.connect(mongourl);
+    // Start the server
+    console.log("conneted to MongoDb Server")
+ app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+    }
+    catch(error)
+    {
+        console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process if the DB connection fails
+    }
+    
+}
+main();
