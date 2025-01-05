fb183b067754f5b0a459d011c4c5446e78c393a2
prakhar362 <prakharshri2005@gmail.com>
2025-01-05 13:04:02 +0530

    navbar part1

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>
+      <div className='flex items-center justify-between px-6  md:px-[200px] bg-black  text-white'>
+      <h1 className="text-lg md:text-xl font-extrabold">Blogosphere</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default navbar
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import Navbar from '../components/navbar'
 function Home() {
   return (
     <div>
+      <Navbar />
       HOME PAGE
     </div>
   )
